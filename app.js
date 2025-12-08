5a953e303b03b900d4bd8228d1fb313aa30e6a24
Dev Gloabal <varunboyapati19@gmail.com>
2025-12-08 12:24:03 +0530

    update the file

---
+++ app.js
@@ -0,0 +1,7 @@
+<html>
+  <body>
+    <h2>JavaScript Main Page</h2>
+    <p id="main">JavaScript changes</p>
+    <button type="button" onclick='document.getElementById("main").innerHTML = "Hey user Welcome!"'>Submit</button>
+  </body>
+</html>
